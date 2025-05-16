903151cf11d41160c9a01b6ba822745da8d03c85
Ojas Mediratta <omediratta3@gatech.edu>
2025-05-16 16:26:00 -0400

    Initial Commit

---
+++ et55/rules.mk
@@ -0,0 +1,6 @@
+# Enable UART API in order to use uLCD 144-G2
+UART_DRIVER_REQUIRED = yes
+# Enable WPM Counter for uLCD display
+WPM_ENABLE = yes
+# Enable Rotary Encoder for volume control
+ENCODER_ENABLE = yes
